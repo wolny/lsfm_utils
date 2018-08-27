511c49bcd62d32fca4d898a2f837dc2304d9e033
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2018-08-27 17:06:31 +0200

    convert script

---
+++ src/convert.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+import h5py
+import numpy
+
+
+def convert_files(data_dir):
+    encoded_data_dir = os.fsencode(data_dir)
+    for file in os.listdir(encoded_data_dir):
+        filename = os.fsdecode(file)
+        segm_path = os.path.join(data_dir, filename)
+        with h5py.File(segm_path, 'r+') as segm_h5:
+            print(f'Converting {segm_path}')
+            dataset = numpy.squeeze(segm_h5['exported_data'])
+            del segm_h5['exported_data']
+            segm_h5.create_dataset(
+                'exported_data',
+                data=dataset,
+                dtype=numpy.uint16,
+                compression='gzip'
+            )
+
+
+if len(sys.argv) != 2:
+    print("Usage: python convert.py <SegmentationDir>")
+    sys.exit(1)
+
+segmented_data_dir = sys.argv[1]
+
+convert_files(segmented_data_dir)
